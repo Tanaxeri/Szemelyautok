dad8219ba5de4076927749f36142a3e898ec2333
Tanaxeri <tamasgamer@gmail.hu>
2022-09-21 10:49:33 +0200

    1.2

---
+++ Szemelyautok/Autok.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szemelyautok
+{
+    class Autok
+    {
+
+        string nev;
+        string tipus;
+        double eve;
+        double ara;
+
+        public string Nev
+        {
+            get => nev;
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+
+                    throw new ArgumentException($"{value} nem lehet az autó neve!");
+
+                }
+                else
+                {
+
+                    nev = value;
+
+                }
+            }
+        }
+        public string Tipus
+        {
+            get => tipus;
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+
+                    throw new ArgumentException($"{value} nem lehet az autó típusa!");
+
+                }
+                else
+                {
+
+                    tipus = value;
+
+                }
+            }
+        }
+        public double Eve
+        {
+            get => eve;
+            set
+            {
+                if (value < 0)
+                {
+
+                    throw new ArgumentException($"{value} nem lehet az autó éve!");
+
+                }
+                else
+                {
+
+                    eve = value;
+
+                }
+            }
+        }
+        public double Ara
+        {
+            get => ara;
+            set
+            {
+                if (value < 0)
+                {
+
+                    throw new ArgumentException($"{value} nem lehet az autó ára!");
+
+                }
+                else
+                {
+
+                    ara = value;
+
+                }
+            }
+        }
+
+
+
+
+        public Autok(string nev, string tipus, double eve, double ara)
+        {
+            this.Nev = nev;
+            this.Tipus = tipus;
+            this.Eve = eve;
+            this.Ara = ara;
+        }
+    }
+}
---
+++ Szemelyautok/Szemelyautok.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szemelyautok
+{
+    class Szemelyautok
+    {
+        static void Main(string[] args)
+        {
+
+            /*Személy autó*/
+            try
+            {
+                Autok auto = new Autok("Audi", "Audi R8", 2000, 21000);
+                Console.WriteLine($"Az autó neve: {auto.Nev}, \ntípusa: {auto.Tipus}, \néve: {auto.Eve}, \nára: {auto.Ara}");
+            }
+            catch (ArgumentException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+
+            }
+            try
+            {
+                Autok auto = new Autok("", "", -2000, -21);
+                Console.WriteLine($"Az autó neve: {auto.Nev}, \ntípusa: {auto.Tipus}, \néve: {auto.Eve}, \nára: {auto.Ara}");
+            }
+            catch (ArgumentException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+
+            }
+
+            Console.WriteLine("\nProgram vége");
+            Console.ReadKey();
+
+        }
+    }
+}
